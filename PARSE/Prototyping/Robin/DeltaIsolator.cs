1604806f7bd227c9519e89fe8b2fc37961df0a36
Robin Janssens <robin.janssens@gmail.com>
2012-10-29 21:49:55 +0000

    set up a few method signatures

--- PARSE/Prototyping/Robin/DeltaIsolator.cs
+++ PARSE/Prototyping/Robin/DeltaIsolator.cs
@@ -8,14 +8,43 @@ namespace PARSE
     class DeltaIsolator
     {
         //I know this is terrible programming practice, but I am just rapidly prototyping, will clean up soon
+        private bool d1Set;
+        private bool d2Set; 
 
+        private byte[] firstDepthFrame;
+        private byte[] secondDepthFrame;
+        
         DeltaIsolator() { 
             //do nothing 
         }
 
-        private void setData() 
-        { 
-            //do nothing
+        private bool setData() 
+        {
+
+            if (!d1Set) {
+                return setFirst();
+            }
+            else if (!d2Set)
+            {
+                return setSecond();
+            }
+
+            //default to failure
+            return false; 
+        }
+
+        private bool setFirst() 
+        {
+
+            //counterfeit atomicity
+            return d1Set = true; 
+        }
+
+        private bool setSecond() 
+        {
+
+            //counterfeit atomicity
+            return d2Set = true; 
         }
 
         private void process() 
