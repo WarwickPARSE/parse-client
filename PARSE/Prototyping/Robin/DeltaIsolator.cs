f39a219a2f1d9e655c3daeba75fd11c8affbeb98
Robin Janssens <robin.janssens@gmail.com>
2012-10-29 21:40:25 +0000

    moving the delta isolator to my prototyping folder

---
+++ PARSE/Prototyping/Robin/DeltaIsolator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PARSE
+{ 
+    class DeltaIsolator
+    {
+        //I know this is terrible programming practice, but I am just rapidly prototyping, will clean up soon
+
+        DeltaIsolator() { 
+            //do nothing 
+        }
+
+        private void setData() 
+        { 
+            //do nothing
+        }
+
+        private void process() 
+        { 
+            //do nothing
+        }
+    }
+}
