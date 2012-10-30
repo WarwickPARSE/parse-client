49680d787665ccf9714a54644150e9624fbdf9d5
Robin Janssens <robin.janssens@gmail.com>
2012-10-30 00:15:11 +0000

    removal of unnecessary imports

--- PARSE/Prototyping/Robin/DeltaIsolator.cs
+++ PARSE/Prototyping/Robin/DeltaIsolator.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace PARSE
 { 
