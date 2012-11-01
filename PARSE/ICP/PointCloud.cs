3bd979158566c837cb4783e13d798ee404a86922
Robin Janssens <robin.janssens@gmail.com>
2012-11-01 15:40:30 +0000

    Addition of a new PointCloud class which will contain some data structure with all of the points in. There will then be methods for querying this data structure etc. WIP

---
+++ PARSE/ICP/PointCloud.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PARSE.ICP
+{
+    class PointCloud
+    {
+
+        //TODO: some implementation of KD tree or octree 
+    }
+}
