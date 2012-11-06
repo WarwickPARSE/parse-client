3cd2089528ebfb63cd9229db27a028648ac5cb93
Robin Janssens <robin.janssens@gmail.com>
2012-11-06 21:58:50 +0000

    modification of constructor API so that point arrays are not necessary on instantiation

--- PARSE/ICP/PointCloud.cs
+++ PARSE/ICP/PointCloud.cs
@@ -29,13 +29,7 @@ namespace PARSE.ICP
         /// Constructor for when just the arrays of x, y and z coordinates are provided.
         /// The resolution will be assumed to be 640 x 480
         /// </summary>
-        /// <param name="x">array of x depth values</param>
-        /// <param name="y">array of y depth values</param>
-        /// <param name="z">array of z depth values</param>
-        public PointCloud(int[] x, int[] y, int[] z) {
-            this.x = x;
-            this.y = y;
-            this.z = y;
+        public PointCloud() {
             this.width = 640;
             this.height = 480;
         }
@@ -44,18 +38,27 @@ namespace PARSE.ICP
         /// Constructor for when just the arrays of x, y and z coordinates are provided.
         /// The resolution will be assumed to be 640 x 480
         /// </summary>
-        /// <param name="x">array of x depth values</param>
-        /// <param name="y">array of y depth values</param>
-        /// <param name="z">array of z depth values</param>
         /// <param name="pixels">the number of pixels (columns)</param>
         /// <param name="rows">the number of rows of pixels</param>
-        public PointCloud(int[] x, int[] y, int[] z, int width, int height)
+        public PointCloud(int width, int height)
         {
-            this.x = x;
-            this.y = y;
-            this.z = y;
             this.width = width;
             this.height = height;
+        }
+
+        public void setX(int[] x) 
+        {
+            this.x = x;
+        }
+
+        public void setY(int[] y) 
+        {
+            this.y = y;
+        }
+
+        public void setZ(int[] z) 
+        {
+            this.z = z;
         }
 
         /// <summary>
