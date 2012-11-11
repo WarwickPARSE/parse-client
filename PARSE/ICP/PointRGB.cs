9b8ca32ac3d250f59b2546bb7dae68eda6e97c32
Robin Janssens <robin.janssens@gmail.com>
2012-11-11 18:04:23 +0000

    addition of new struct for when we decide to add colour to the pcd

---
+++ PARSE/ICP/PointRGB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PARSE.ICP
+{
+    public struct PointRGB
+    {
+        public int x, y, z;
+        public int r, g, b;
+
+        public PointRGB(int x, int y, int z, int r, int g, int b)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+}
