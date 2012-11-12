cf064b5a2fd63f0e245c5a3e2ad71f6a7c455a0e
Robin Janssens <robin.janssens@gmail.com>
2012-11-12 18:10:11 +0000

    addition of multithreading component

--- PARSE/PointCloudHandler.cs
+++ PARSE/PointCloudHandler.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Text;
 
 namespace PARSE
 {
     class PointCloudHandler
     {
+        private int frequency;          //frequency in milliseconds
+        private bool scanning;          //are we actually scanning the point cloud?
+
+
         public PointCloudHandler(int frequency) 
-        { 
-        
+        {
+            this.frequency = frequency; 
         }
 
         public void run() 
         {
-            //TODO
+            while (scanning) 
+            {
+                //do some point cloud icp jiggery pokery here 
+
+                //done, sleep and wait for the next scan
+                Thread.Sleep(frequency);
+            }
         }
     }
 }
