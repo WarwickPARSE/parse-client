8563a5692b5d5d86a90e4c0825cc85298e8f2b0e
Robin Janssens <robin.janssens@gmail.com>
2012-11-22 21:19:19 +0000

    implementation of image resizer

--- PARSE/Library/ImageResizer.cs
+++ PARSE/Library/ImageResizer.cs
@@ -9,5 +9,22 @@ namespace PARSE.Library
 {
     class ImageResizer
     {
+        public static System.Drawing.Bitmap ResizeImage(System.Drawing.Bitmap image, int width, int height) 
+        {
+            Bitmap result = new Bitmap(width, height);
+
+            using (Graphics graphics = Graphics.FromImage(result)) 
+            {
+                //try to reduce the loss of quality 
+                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode =System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                //draw image into bitmap
+                graphics.DrawImage(image, 0, 0, result.Width, result.Height);
+            }
+
+            return result; 
+        }
     }
 }
