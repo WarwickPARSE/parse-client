d1a01159dfaff3759f3597ae56dd5e9efdbd0ec8
Janssens <robin.janssens@gmail.com>
2013-01-29 16:30:56 +0000

    ignore this

--- PARSE/ICP/ICP3D.cs
+++ PARSE/ICP/ICP3D.cs
@@ -7,5 +7,24 @@ namespace PARSE.ICP
 {
     class ICP3D
     {
+        public void initialiseAllignment(/*Scene, Model*/)
+        { 
+        
+        }
+
+        public void applyAllignment(/*Scene, Rot, Trans*/)
+        { 
+        
+        }
+
+        public void returnClosestPairs(/*Alligned scene, Model*/) 
+        { 
+        
+        }
+
+        public void updateAllignment(/**/) 
+        { 
+        
+        }
     }
 }
