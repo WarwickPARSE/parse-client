239c85f6c8374a37b3f19410678822d45c2d0a96
Bernard Sexton <b.sexton@warwick.ac.uk>
2013-01-29 16:45:01 +0000

    added skeletal methods for volcalc

--- PARSE/Measurement/VolumeCalculator.cs
+++ PARSE/Measurement/VolumeCalculator.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Emgu.CV.CvEnum;
 
 namespace PARSE
 {
-    static class VolumeCalculator
+    public static class VolumeCalculator
     {
-        
+        private static double calculateVolume(KdTree.KDTree pctree)
+        {
+
+            return 0;
+        }
+
+        private static double partitionKDTree(KdTree.KDTree pctree)
+        {
+
+
+            return 0;
+        }
+
+        private static double computeConvexHull(Object[] pclayer)
+        {
+
+            return 0;
+        }
+
     }
 }
