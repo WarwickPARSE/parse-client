fb53f35fbec9d946af71218e563fca41d6595063
Janssens <robin.janssens@gmail.com>
2013-02-01 16:04:21 +0000

    removed visualisation shiz from pointcloud class

--- PARSE/ICP/PointCloud.cs
+++ PARSE/ICP/PointCloud.cs
@@ -21,13 +21,13 @@ namespace PARSE
     class PointCloud
     {
         //dodgy global variables (to be changed)
-        private int width;
-        private int height;
+        //private int width;
+        //private int height;
 
         //tree of points
         private KdTree.KDTree points;
 
-        private bool is_dense;                  //to be calculated
+        //private bool is_dense;                  //to be calculated
 
         //depth constants
         private const int tooCloseDepth = 0;
@@ -46,7 +46,7 @@ namespace PARSE
         //fiddle values (they make it work but I don't know why!)
         private const double fxinv = 1.0 / 476;
         private const double fyinv = 1.0 / 476;
-        private double ddt = 200;
+        //private double ddt = 200;
 
         //bitmap source, to be deprecated soon
         BitmapSource bs; 
@@ -63,34 +63,6 @@ namespace PARSE
         //sensor_orientation
         //sensor_origin
 
-        /// <summary>
-        /// Constructor for when just the arrays of x, y and z coordinates are provided.
-        /// The resolution will be assumed to be 640 x 480
-        /// </summary>
-        public PointCloud() {
-            this.width = 640;
-            this.height = 480;
-
-            //create a new 3d point cloud
-            this.points = new KdTree.KDTree(3);
-        }
-
-        /// <summary>
-        /// Constructor for when just the arrays of x, y and z coordinates are provided.
-        /// The resolution will be assumed to be 640 x 480
-        /// </summary>
-        /// <param name="pixels">the number of pixels (columns)</param>
-        /// <param name="rows">the number of rows of pixels</param>
-        public PointCloud(int width, int height)
-        {
-            this.width = width;
-            this.height = height;
-
-            //create a new 3d point cloud 
-            this.points = new KdTree.KDTree(3);
-        }
-
-
         public PointCloud(BitmapSource bs, int[] rawDepth) 
         {
             this.bs = bs;
@@ -99,14 +71,6 @@ namespace PARSE
             textureCoordinates = new System.Windows.Point[depthFrameHeight * depthFrameWidth];
             depthFramePoints = new Point3D[depthFrameHeight * depthFrameWidth];
 
-            this.legacy();
-
-            //create mesh default 
-            this.Model.Geometry = createMesh();
-            this.Model.Material = this.Model.BackMaterial = new DiffuseMaterial(new ImageBrush(this.bs));
-            this.Model.Transform = new TranslateTransform3D(1, -2, 1);
-
-            //this is pointless if you are instantiating in this manner, but meh 
             this.points = new KdTree.KDTree(3);
         }
 
@@ -263,24 +227,9 @@ namespace PARSE
         {
             return this.points;
         }
-
-        /***
-         * Aids in the transition from Bernie's point cloud to this one 
-         */
-        public void legacy()
-        {
-            //sanity check for helix responsiveness 
-            this.runDemoModel();
-
-            //create the texture coords
-            this.createTexture();
-
-            //create the depth coords
-            this.createDepthCoords();
-        }
         
         /***
-         * Creates tecture coordinates  
+         * Creates texture coordinates  
          */
         private void createTexture()
         {
@@ -328,80 +277,5 @@ namespace PARSE
             }
         }
 
-        public MeshGeometry3D createMesh()
-        {
-            var triangleIndices = new List<int>();
-            for (int iy = 0; iy + 1 < depthFrameHeight; iy++)
-            {
-                for (int ix = 0; ix + 1 < depthFrameWidth; ix++)
-                {
-                    int i0 = (iy * depthFrameWidth) + ix;
-                    int i1 = (iy * depthFrameWidth) + ix + 1;
-                    int i2 = ((iy + 1) * depthFrameWidth) + ix + 1;
-                    int i3 = ((iy + 1) * depthFrameWidth) + ix;
-
-                    var d0 = this.rawDepth[i0];
-                    var d1 = this.rawDepth[i1];
-                    var d2 = this.rawDepth[i2];
-                    var d3 = this.rawDepth[i3];
-
-                    var dmax0 = Math.Max(Math.Max(d0, d1), d2);
-                    var dmin0 = Math.Min(Math.Min(d0, d1), d2);
-                    var dmax1 = Math.Max(d0, Math.Max(d2, d3));
-                    var dmin1 = Math.Min(d0, Math.Min(d2, d3));
-
-                    if (dmax0 - dmin0 < ddt && dmin0 != -1)
-                    {
-                        triangleIndices.Add(i0);
-                        triangleIndices.Add(i1);
-                        triangleIndices.Add(i2);
-                    }
-
-                    if (dmax1 - dmin1 < ddt && dmin1 != -1)
-                    {
-                        triangleIndices.Add(i0);
-                        triangleIndices.Add(i2);
-                        triangleIndices.Add(i3);
-                    }
-                }
-            }
-
-            return new MeshGeometry3D()
-            {
-                Positions = new Point3DCollection(this.depthFramePoints),
-                TextureCoordinates = new System.Windows.Media.PointCollection(this.textureCoordinates),
-                TriangleIndices = new Int32Collection(triangleIndices)
-            };
-        }
-
-        /***
-         * Some sanity check 
-         */
-        private void runDemoModel()
-        {
-            // Create a mesh builder and add a box to it
-            var meshBuilder = new MeshBuilder(false, false);
-            meshBuilder.AddBox(new Point3D(0, 0, 1), 1, 2, 0.5);
-            meshBuilder.AddBox(new Rect3D(0, 0, 1.2, 0.5, 1, 0.4));
-
-            // Create a mesh from the builder (and freeze it)
-            var mesh = meshBuilder.ToMesh(true);
-
-            // Create some materials
-            var greenMaterial = MaterialHelper.CreateMaterial(Colors.Green);
-
-            this.Model = new GeometryModel3D { Geometry = mesh, Transform = new TranslateTransform3D(0, 0, 0), Material = greenMaterial, BackMaterial = greenMaterial };
-           // this.BaseModel = new GeometryModel3D { Geometry = mesh, Transform = new TranslateTransform3D(0, 0, 0), Material = greenMaterial, BackMaterial = greenMaterial };
-        }
-
-        /// <summary>
-        /// Gets or sets the sanity model.
-        /// </summary>
-        /// <value>The model.</value>
-
-        public GeometryModel3D Model { get; set; }
-        public GeometryModel3D BaseModel { get; set; }
-
-        //
     }
 }
