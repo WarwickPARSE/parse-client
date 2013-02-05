6b08d9e74100d705bb032f01203f330cea2eeeb2
Janssens <robin.janssens@gmail.com>
2013-02-05 16:20:14 +0000

    refactored into point3D to bring into line with Greg's changes

--- PARSE/ICP/PointRGB.cs
+++ PARSE/ICP/PointRGB.cs
@@ -2,23 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Media.Media3D;
 
 namespace PARSE.ICP
 {
     public struct PointRGB
     {
-        public double x { get; set; }
-        public double y { get; set; }
-        public double z { get; set; }
+        public Point3D point { get; set; }
+
         public double r { get; set; }
         public double g { get; set; }
         public double b { get; set; }
 
         public PointRGB(double x, double y, double z, double r, double g, double b)
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            this.point = new Point3D(x, y, z);
 
             this.r = r;
             this.g = g;
