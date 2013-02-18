d720283b517a16fdd3d702227a0f2b4284e85a3d
Janssens <robin.janssens@gmail.com>
2013-02-18 17:29:55 +0000

    bounding box: fully overridden abstract methods

--- PARSE/ICP/Stitchers/BoundingBox.cs
+++ PARSE/ICP/Stitchers/BoundingBox.cs
@@ -5,7 +5,54 @@ using System.Text;
 
 namespace PARSE.ICP.Stitchers
 {
-    class BoundingBox
+    class BoundingBox : Stitcher
     {
+        List<PointCloud> pointClouds;
+        PointCloud pcd;
+
+        /// <summary>
+        /// Instantiates an empty point cloud, ready for us to dump some data
+        /// </summary>
+        public BoundingBox() {
+            //instantiate empty lists and point clouds 
+            pointClouds = new List<PointCloud>();
+            pcd = new PointCloud();
+        }
+
+        /// <summary>
+        /// Add a point cloud into the list of point clouds
+        /// </summary>
+        /// <param name="pc">Input point cloud</param>
+        public override void add(PointCloud pc)
+        {
+            this.pointClouds.Add(pc);
+        }
+
+        /// <summary>
+        /// Add a list of point clouds to the list of point clouds
+        /// </summary>
+        /// <param name="pcs"></param>
+        public override void add(List<PointCloud> pcs)
+        {
+            this.pointClouds = pcs;
+            Console.WriteLine("There are " + pcs.Count);
+        }
+
+        /// <summary>
+        /// Stitch the point clouds using the "bounding box" algorithm
+        /// </summary>
+        public void stitch() { 
+        
+        }
+
+        /// <summary>
+        /// Return the result of the stitching
+        /// </summary>
+        /// <returns>The result of the stitching</returns>
+        public override PointCloud getResult()
+        {
+            return pcd;
+        }
+
     }
 }
