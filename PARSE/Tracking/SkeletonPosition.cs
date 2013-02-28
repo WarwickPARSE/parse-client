01e27949294f73cd292bb3b8b7d25022861772c0
amoebanath <n.page@warwick.ac.uk>
2013-02-28 16:07:24 +0000

    Set up relative position capture function for Stef. Some progress towards workflow.

---
+++ PARSE/Tracking/SkeletonPosition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace PARSE.Tracking
+{
+    class SkeletonPosition
+    {
+        public SkeletonPoint point1 {get; set;}
+        public SkeletonPoint point2 {get; set;}
+        public SkeletonPoint point3 {get; set;}
+
+        public float anglexy { get; set; }
+        public float anglez { get; set; }
+
+        public SkeletonPosition()
+        {
+            
+        }
+
+    }
+}
