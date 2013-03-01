ff25d2a348dda070aef69cf6a3d69b22c791fedf
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-01 16:02:49 +0000

    constant fussed with

--- PARSE/Measurement/UnitConvertor.cs
+++ PARSE/Measurement/UnitConvertor.cs
@@ -7,9 +7,9 @@ namespace PARSE
 {
     class UnitConvertor
     {
-        private const double pctorwtransform = 0.554692801;//to be determined experimentally for 3 dimensional content such as volume.
+        private const double pctorwtransform = 0.548984131;//to be determined experimentally for 3 dimensional content such as volume.
         
-        public static double convertPCVolumeMeasurement(double PCM)
+        public static double convertPC3DMeasurement(double PCM)
         {
             double output = -1;
             output = PCM * pctorwtransform;
@@ -17,10 +17,17 @@ namespace PARSE
 
         }
 
-        public static double convertPCVHeightMeasurement(double PCM)
+        public static double convertPC1DMeasurement(double PCM)
         {
             double output = -1;
-            output = PCM * Math.Pow(pctorwtransform,1/3);//needs to be cube rooted because the constant is for 3 dimensions and convertPCVHeightMeasurement is 1d
+            output = PCM * Math.Pow(pctorwtransform,1/3);//needs to be cube rooted because the constant is for 3 dimensions and convertPC1D is 1d
+            return output;
+        }
+
+        public static double convertPC2DMeasurement(double PCM)
+        {
+            double output = -1;
+            output = PCM * Math.Pow(pctorwtransform, 2 / 3);//needs to be cube rooted then squared because the constant is for 3 dimensions and convertPC2D is 2d
             return output;
         }
 
