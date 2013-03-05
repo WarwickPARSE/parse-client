5c604d6e3e11d425031d12d2a621fbbe62b5007a
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-05 16:02:39 +0000

    class added

---
+++ PARSE/Measurement/AreaCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Media3D;
+
+namespace PARSE
+{
+    static class AreaCalculator
+    {
+        public static double calculateArea(List<Point3D> plane)
+        {
+            double area = 0;
+            for (int j = 0; j < plane.Count - 1; j++)
+            {
+               area = area + ((plane[j].X * plane[j + 1].Z) - (plane[j + 1].X * plane[j].Z));
+            }
+            return area;
+        }
+    }
+}
