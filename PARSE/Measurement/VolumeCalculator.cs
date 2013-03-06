f6619ec8c49d4711711937a9bae7261a5fe651e3
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-06 13:44:37 +0000

    less console spaff, less comment spaff

--- PARSE/Measurement/VolumeCalculator.cs
+++ PARSE/Measurement/VolumeCalculator.cs
@@ -13,25 +13,6 @@ namespace PARSE
     {
         private const int number = 60;
 
-        /*private static double getBoundingBoxVolume(double xmin, double xmax, double ymin, double ymax, double zmin, double zmax)
-        {
-            return ((xmax - xmin) * (ymax - ymin) * (zmax - zmin));
-        }
-
-        private static double volume0thApprox(PointCloud pc)
-        {
-            double xmin = pc.getxMin();
-            double xmax = pc.getxMax();
-            double ymin = pc.getyMin();
-            double ymax = pc.getyMax();
-            double zmin = pc.getzMin();
-            double zmax = pc.getzMax();
-            double volume = getBoundingBoxVolume(xmin,xmax,ymin,ymax,zmin,zmax);
-            //System.Diagnostics.Debug.WriteLine("Volume Cock Multi: " + volume);
-            volume = UnitConvertor.convertPCM(volume);
-            return volume;
-        }*/
-
         //only works on an amorphus blob
         public static Tuple<double, List<List<Point3D>>> volume1stApprox(PointCloud pc)
         {
@@ -53,7 +34,6 @@ namespace PARSE
                 List<Point3D> plane = pc.getKDTree().getAllPointsAt(i, increment / 2, limits);
                 if (plane.Count != 0)
                 {
-                    Console.WriteLine("Plane is not empty");
                     plane = PointSorter.rotSort(plane);
                     planes.Add(plane);
                     plane.Add(plane[0]); //a list eating its own head, steve matthews would be proud
@@ -62,8 +42,6 @@ namespace PARSE
 
                     area = AreaCalculator.calculateArea(plane);
                     
-                    Console.WriteLine("Area2:" + UnitConvertor.convertPC2DMeasurement(area));
-
                     volume = volume + (area * increment);
                 }
                 else
@@ -75,11 +53,5 @@ namespace PARSE
             volume = UnitConvertor.convertPC3DMeasurement(volume);
             return Tuple.Create(volume,planes);
         }
-        
-        /*public static double calculateVolume(PointCloud pc)
-        {
-            System.Diagnostics.Debug.WriteLine("Upper PENIS Bound on Patient Volume: " + volume0thApprox(pc));
-            return 0;//volume1stApprox(pc);
-        }*/
     }
 }
