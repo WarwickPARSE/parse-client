d2b74c3ac9035e3d10ac9d3a86201cc50e88a075
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-06 16:42:27 +0000

    graphs!

--- PARSE/RuntimeLoader.xaml.cs
+++ PARSE/RuntimeLoader.xaml.cs
@@ -196,6 +196,15 @@ namespace PARSE
                 System.Diagnostics.Debug.WriteLine("zmin: " + zmin);
                 System.Diagnostics.Debug.WriteLine("xmax: " + xmax);
                 System.Diagnostics.Debug.WriteLine("zmax: " + zmax);
+
+                //setData
+                ((LineSeries)(volchart.Series[0])).ItemsSource =
+                    new KeyValuePair<DateTime,int>[]{
+                    new KeyValuePair<DateTime,int>(DateTime.Now, 100),
+                    new KeyValuePair<DateTime,int>(DateTime.Now.AddMonths(1), 130),
+                    new KeyValuePair<DateTime,int>(DateTime.Now.AddMonths(2), 150),
+                    new KeyValuePair<DateTime,int>(DateTime.Now.AddMonths(3), 125),
+                    new KeyValuePair<DateTime,int>(DateTime.Now.AddMonths(4),155) };
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
