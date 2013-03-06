f030bee44f6f0ecba57cad077e1a73567c456b3a
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-06 13:08:13 +0000

    comments added for when you need the pcs height

--- PARSE/Measurement/HeightCalculator.cs
+++ PARSE/Measurement/HeightCalculator.cs
@@ -12,7 +12,9 @@ namespace PARSE
             double ymin = pc.getyMin();
             double ymax = pc.getyMax();
             double height = ymax - ymin;
+            //Console.WriteLine(height);
             height = UnitConvertor.convertPC1DMeasurement(height);
+            //Environment.Exit(1);
             return height;
         }
     }
