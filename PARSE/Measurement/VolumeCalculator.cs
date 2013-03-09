2733a65127b08d980041315bae297599361a1ed0
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-09 20:54:48 +0000

    refactored to take planes

--- PARSE/Measurement/VolumeCalculator.cs
+++ PARSE/Measurement/VolumeCalculator.cs
@@ -11,30 +11,19 @@ namespace PARSE
 {
     public static class VolumeCalculator
     {
-        public const int number = 60;
+        
 
         //only works on an amorphus blob
-        public static Tuple<double, List<List<Point3D>>> volume1stApprox(PointCloud pc)
+        public static double volume1stApprox(List<List<Point3D>> planes, double increment)
         {
-            double xmin = pc.getxMin();
-            double xmax = pc.getxMax();
-            double zmin = pc.getzMin();
-            double zmax = pc.getzMax();
-            double[] limits = { xmin, zmin, xmax, zmax };
-
-            double ymin = pc.getyMin();
-            double ymax = pc.getyMax();
-            double increment = (ymax - ymin) / number;
+            
             double volume = 0;
-            List<List<Point3D>> planes = new List<List<Point3D>>();
-
-            for (double i = ymin + (increment / 2); i <= ymax - (increment / 2); i = i + increment)
+            for (int i = 0; i < planes.Count; i++)
             {
-                List<Point3D> plane = pc.getKDTree().getAllPointsAt(i, increment / 2, limits);
+                List<Point3D> plane = planes[i]; 
                 if (plane.Count != 0)
                 {
                     plane = PointSorter.rotSort(plane);
-                    planes.Add(plane);
                     plane.Add(plane[0]); //a list eating its own head, steve matthews would be proud
 
                     double area = 0;
@@ -46,11 +35,10 @@ namespace PARSE
                 else
                 {
                     Console.WriteLine("Plane EMPTY!!! BAD THINGS WILL HAPPEN");
-                    //Environment.Exit(-1);
                 }
             }
             volume = UnitConvertor.convertPCM(volume,3);
-            return Tuple.Create(volume,planes);
+            return volume;
         }
     }
 }
