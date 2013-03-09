3eb3cf4a25de60fecb41532e094737e8a9cd8f49
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-09 20:43:47 +0000

    cant get away from that tuple :(

--- PARSE/Measurement/PlanePuller.cs
+++ PARSE/Measurement/PlanePuller.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Media.Media3D;
 
-namespace PARSE.Measurement
+namespace PARSE
 {
-    class PlanePuller
+    static class PlanePuller
     {
+        public const int number = 60;
+        
+        public static Tuple<List<List<Point3D>>,double> pullAll(PointCloud pc)
+        {
+            double xmin = pc.getxMin();
+            double xmax = pc.getxMax();
+            double zmin = pc.getzMin();
+            double zmax = pc.getzMax();
+            double[] limits = { xmin, zmin, xmax, zmax };
+
+            double ymin = pc.getyMin();
+            double ymax = pc.getyMax();
+            double increment = (ymax - ymin) / number;
+
+            List<List<Point3D>> output = new List<List<Point3D>>();
+
+            for (double i = ymin + (increment / 2); i <= ymax - (increment / 2); i = i + increment)
+            {
+                output.Add(pc.getKDTree().getAllPointsAt(i, increment / 2, limits));
+            }
+            
+            return Tuple.Create(output,increment);
+        }
     }
 }
