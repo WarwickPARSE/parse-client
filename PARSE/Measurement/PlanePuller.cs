81c0a8807d5350e8d6aeabfbe83b49192c99f8ce
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-09 21:25:20 +0000

    god damn it

--- PARSE/Measurement/PlanePuller.cs
+++ PARSE/Measurement/PlanePuller.cs
@@ -26,9 +26,11 @@ namespace PARSE
 
             for (double i = ymin + (increment / 2); i <= ymax - (increment / 2); i = i + increment)
             {
-                output.Add(pc.getKDTree().getAllPointsAt(i, increment / 2, limits));
+                List<Point3D> plane = pc.getKDTree().getAllPointsAt(i, increment / 2, limits);
+                //plane = ConvexHullFinder.findHull(plane);
+                output.Add(plane);
             }
-            
+
             return Tuple.Create(output,increment);
         }
     }
