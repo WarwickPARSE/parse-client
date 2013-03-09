00d5091676cf428db28aa85269a9f210565cbfd5
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-09 19:27:36 +0000

    Volume Calculator WILL CRASH!!! Bernard told me to do it!

--- PARSE/Measurement/LimbCalculator.cs
+++ PARSE/Measurement/LimbCalculator.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Media.Media3D;
 using Microsoft.Kinect;
+using Emgu.CV;
+using System.Drawing;
 
 namespace PARSE
 {
@@ -15,9 +17,26 @@ namespace PARSE
         {
             double circum = 0;
 
-            for (int j = 0; j < planes[planeNo].Count - 1; j++)
+            List<Point3D> plane3D = planes[planeNo];
+            PointF[] planeF = new PointF[plane3D.Count];
+
+            for (int i = 0; i < plane3D.Count; i++)
             {
-                circum = circum + Math.Sqrt(Math.Pow((planes[planeNo][j + 1].X - planes[planeNo][j].X), 2) + Math.Pow((planes[planeNo][j + 1].Y - planes[planeNo][j].Y), 2));
+                planeF[i] = new PointF((float) plane3D[i].X,(float) plane3D[i].Z);
+            }
+
+            MemStorage storage = new MemStorage();
+            planeF = PointCollection.ConvexHull(planeF, storage, Emgu.CV.CvEnum.ORIENTATION.CV_CLOCKWISE).ToArray();
+            
+            plane3D = new List<Point3D>();
+            for (int i = 0; i < planeF.Length; i++)
+            {
+                plane3D.Add(new Point3D(planeF[i].X,0,planeF[i].Y));
+            }
+
+            for (int j = 0; j < plane3D.Count - 1; j++)
+            {
+                circum = circum + Math.Sqrt(Math.Pow((plane3D[j + 1].X - plane3D[j].X), 2) + Math.Pow((plane3D[j + 1].Y - plane3D[j].Y), 2));
             }
 
             Console.WriteLine("Circum Pre Multi: " + circum);
