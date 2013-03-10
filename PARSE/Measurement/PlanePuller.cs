b2f30b2859eb6fc849a1c447b82886ed2453230d
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-10 14:59:00 +0000

    better than 25,4

--- PARSE/Measurement/PlanePuller.cs
+++ PARSE/Measurement/PlanePuller.cs
@@ -27,8 +27,8 @@ namespace PARSE
             for (double i = ymin + (increment / 2); i <= ymax - (increment / 2); i = i + increment)
             {
                 List<Point3D> plane = pc.getKDTree().getAllPointsAt(i, increment / 2, limits);
-                plane = SubSampler.subSample(plane,25);
-                plane = SubSampler.averageSample(plane,4);
+                plane = SubSampler.subSample(plane,5);
+                plane = SubSampler.averageSample(plane,5);
                 output.Add(plane);
             }
 
