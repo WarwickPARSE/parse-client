816351f7bb344e4a474c1880fafeaacf38c18c4f
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-10 13:41:25 +0000

    class added

---
+++ PARSE/Measurement/SubSampler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Media3D;
+
+namespace PARSE.Measurement
+{
+    class SubSampler
+    {
+        static List<Point3D> subSample(List<Point3D> input, int factor)
+        {
+            List<Point3D> output = new List<Point3D>();
+            return output;
+        }
+    }
+}
