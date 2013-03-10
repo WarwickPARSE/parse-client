4f50614eb12a071c189346857823bf9d0d2202bc
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-10 12:05:41 +0000

    class added

---
+++ PARSE/Measurement/GiftWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Media3D;
+
+namespace PARSE
+{
+    static class GiftWrapper
+    {
+        public static List<Point3D> wrap(List<Point3D> input)
+        {
+            List<Point3D> output = new List<Point3D>();
+
+
+
+            return output;
+        }
+    }
+}
