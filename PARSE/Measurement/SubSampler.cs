b1dd14c9ec1936c2c4f1f8c72c1f58492dd96561
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-10 14:11:20 +0000

    average method added

--- PARSE/Measurement/SubSampler.cs
+++ PARSE/Measurement/SubSampler.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Windows.Media.Media3D;
 
-namespace PARSE.Measurement
+namespace PARSE
 {
-    class SubSampler
+    static class SubSampler
     {
-        static List<Point3D> subSample(List<Point3D> input, int factor)
+        public static List<Point3D> subSample(List<Point3D> input, int factor)
+        {
+            List<Point3D> output = new List<Point3D>();
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (i % factor == 0)
+                {
+                    output.Add(input[i]);
+                }
+            }
+            return output;
+        }
+
+        public static List<Point3D> averageSample(List<Point3D> input, int factor)
         {
             List<Point3D> output = new List<Point3D>();
             for (int i = 0; i < input.Count; i++)
