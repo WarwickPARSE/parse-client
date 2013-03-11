1ad9f7a9ab8a19e1d70064b724f8f8deeb5d33b3
Janssens <robin.janssens@gmail.com>
2013-03-11 11:46:51 +0000

    creation of a point cloud class to encapsulate data between the "interpreter" and kd-tree

---
+++ PARSE/Library/PointCloud2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PARSE.Library
+{
+    class PointCloud2
+    {
+        //width and height of a point cloud
+        int height;
+        int width;
+        
+        int point_step; //length of a point in bytes 
+        int row_step;   //length of a row in bytes
+
+        int[] data;     //the depth data
+
+        bool is_dense;  //are all points valid?  
+    }
+}
