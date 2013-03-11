a5cea24e678607d0d04deee1bc4c702444fad53c
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-03-11 12:23:31 +0000

    faf

--- PARSE/Measurement/HeightCalculator.cs
+++ PARSE/Measurement/HeightCalculator.cs
@@ -12,7 +12,7 @@ namespace PARSE
             double ymin = pc.getyMin();
             double ymax = pc.getyMax();
             double height = ymax - ymin;
-            //Console.WriteLine(height);
+            Console.WriteLine(height);
             height = UnitConvertor.convertPCM(height,1);
             //Environment.Exit(1);
             return height;
