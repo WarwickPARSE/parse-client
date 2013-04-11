af9ca90ef246e2557a7e14521cb842c24ff0f9c6
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-04-11 13:21:47 +0100

    think this is the last one

--- PARSE/ICP/SubRegionOutOfBoundsException.cs
+++ PARSE/ICP/SubRegionOutOfBoundsException.cs
@@ -6,11 +6,18 @@ namespace PARSE
     {
         public String type { get; private set; }
         
+        /// <summary>
+        /// creates an exception with no additional information
+        /// </summary>
         public SubRegionOutOfBoundsException()
         {
             Console.WriteLine("Somethings gone all wibbly.");
         }
 
+        /// <summary>
+        /// creates an exception with a type S
+        /// </summary>
+        /// <param name="S">String</param>
         public SubRegionOutOfBoundsException(String S)
         {
             Console.WriteLine(S+" has gone all wibbly.");
