b0b6bc61e774a3d70b1f9c0bc1abaffcce1b7f65
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2013-04-18 13:51:12 +0100

    cannot check a newly plugged in kinect :(

--- PARSE/CoreLoader.xaml.cs
+++ PARSE/CoreLoader.xaml.cs
@@ -101,21 +101,7 @@ namespace PARSE
             
             kinectInterp = new KinectInterpreter(vpcanvas2);
 
-            if (kinectInterp.errorFlag)
-            {
-                switch (kinectInterp.kinectSensor.Status)
-                {
-                    case KinectStatus.DeviceNotGenuine: sandra.Speak("Your Kinect is not genuine"); break;
-                    case KinectStatus.DeviceNotSupported: sandra.Speak("Your Kinect is not supported"); break;
-                    case KinectStatus.InsufficientBandwidth: sandra.Speak("Your USB connection has insufficient Bandwidth "); break;
-                    case KinectStatus.NotPowered: sandra.Speak("Your Kinect is not powered"); break;
-                    case KinectStatus.NotReady: sandra.Speak("Your Kinect is not ready"); break;
-                    case KinectStatus.Error: sandra.Speak("Your Kinect is has errored in some way"); break;
-                    case KinectStatus.Undefined: sandra.Speak("Your Kinect is has errored in an undefined way"); break;
-                    default: sandra.Speak("Call Technical Support."); break;
-                }	
-            Environment.Exit(1);
-            }
+            this.checkKinectIsOkay();
 
             this.kinectmenu.IsEnabled = false;
             this.newscan.IsEnabled = false;
@@ -129,6 +115,25 @@ namespace PARSE
 
             this.resetButtons();
 
+        }
+
+        private void checkKinectIsOkay()
+        {
+            if (kinectInterp.errorFlag)
+            {
+                switch (kinectInterp.kinectSensor.Status)
+                {
+                    case KinectStatus.DeviceNotGenuine: sandra.Speak("Your Kinect is not genuine"); break;
+                    case KinectStatus.DeviceNotSupported: sandra.Speak("Your Kinect is not supported"); break;
+                    case KinectStatus.InsufficientBandwidth: sandra.Speak("Your USB connection has insufficient Bandwidth "); break;
+                    case KinectStatus.NotPowered: sandra.Speak("Your Kinect is not powered"); break;
+                    case KinectStatus.NotReady: sandra.Speak("Your Kinect is not ready"); break;
+                    case KinectStatus.Error: sandra.Speak("Your Kinect is has errored in some way"); break;
+                    case KinectStatus.Undefined: sandra.Speak("Your Kinect is has errored in an undefined way"); break;
+                    default: sandra.Speak("Call Technical Support."); break;
+                }
+                Environment.Exit(1);
+            }
         }
 
         /// <summary>
