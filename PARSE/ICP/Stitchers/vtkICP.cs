ce0df43e40860f3eb11c795d416109743ba4fa97
Janssens <robin.janssens@gmail.com>
2013-09-12 19:14:55 +0100

    lol

--- PARSE/ICP/Stitchers/vtkICP.cs
+++ PARSE/ICP/Stitchers/vtkICP.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Kitware.mummy;
+using Kitware.VTK; 
+
 namespace PARSE.ICP.Stitchers
 {
     class vtkICP : Stitcher
@@ -11,6 +14,10 @@ namespace PARSE.ICP.Stitchers
         List<PointCloud> txpointClouds;
         PointCloud pcd;
 
+        vtkIterativeClosestPointTransform vtkICPAlgo; 
+        vtkPoints sourceData;
+        vtkCellArray sourcePoints; 
+    
         /// <summary>
         /// Instantiates an empty point cloud, ready for us to dump some data
         /// </summary>
@@ -19,14 +26,28 @@ namespace PARSE.ICP.Stitchers
             pointClouds = new List<PointCloud>();
             txpointClouds = new List<PointCloud>();
             pcd = new PointCloud();
-        }
 
-        public override void add(List<PointCloud> pc)
-        {
-            throw new NotImplementedException();
+            //instantiate vtk lists
+            vtkICPAlgo = new vtkIterativeClosestPointTransform();
+            sourceData = new vtkPoints();
+            sourcePoints = new vtkCellArray(); 
         }
 
         public override void add(PointCloud pc)
+        {
+            //stick into the pointcloud list
+            this.pointClouds.Add(pc);
+
+            //if the point cloud list isn't empty, register against previous scan 
+            if (this.pointClouds.Count != 0) {
+                //register against previous scan
+
+                //dump the points from the cloud into the points 
+                
+            }
+        }
+
+        public override void add(List<PointCloud> pc)
         {
             throw new NotImplementedException();
         }
